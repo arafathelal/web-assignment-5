357b816772281481241f5bbc101aa5e7566ecbe2
Arafat <arafathelal7@gmail.com>
2026-03-09 10:43:02 +0600

    tabs and other ui added, api tested

---
+++ issue.js
@@ -0,0 +1,11 @@
+//checking api
+async function loadIssues() {
+
+  const res = await fetch(
+    "https://phi-lab-server.vercel.app/api/v1/lab/issues"
+  );
+  const data = await res.json();
+  console.log(data)
+}
+
+loadIssues();
